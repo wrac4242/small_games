143d147f1a2d868f72665340357d3c111a64cf39
wrac4242 <wrac4242@gmail.com>
2021-07-13 20:18:42 +0100

    add basic game checking into the code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Wrac's set of games");
+    // A basic terminal menu, press numbers to go to the different games
+
+    
+    #[allow(unused_assignments)]
+    let mut game = 0;
+    
+    loop {
+        println!("Press 1 for nim, 2 for blackjack");
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_n) => {
+                let choice = input.trim();
+
+                match choice {
+                    "1" => {
+                        game = 1;
+                        break;
+                    },
+                    "2" => {
+                        game = 2;
+                        break;
+                    },
+                    _ => {
+                        println!("Error, this is not a valid selection\nplease reenter");
+                    }
+                }
+            }
+            Err(error) => {
+                println!("error: {}", error);
+                return;
+            },
+        }
+    }
+    println!("{}", game);
 }
